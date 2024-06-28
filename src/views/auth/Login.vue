2cf4896276137574e2e46baa0335403681f96253
Makara <makarasiv058@gmail.com>
2024-06-28 08:48:55 +0700

    update page login

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,61 +1,39 @@
 <template>
-  <div class="hold-transition login-page">
-    <div class="login-box">
-      <div class="card card-outline card-primary">
-        <div class="card-header text-center">
-          <a href="../../index2.html" class="h1"><b>Admin</b>Page</a>
-        </div>
-        
-        <img src="@/assets/test.jpeg" alt="Login Image">
-    
-        <div class="card-body">
-          <form action="../../index3.html" method="post">
-            <div class="input-group mb-3">
-              <input type="email" class="form-control" placeholder="UserName">
-              <div class="input-group-append">
-                <div class="input-group-text">
-                  <span class="fas fa-envelope"></span>
-                </div>
-              </div>
-            </div>
-            <div class="input-group mb-3">
-              <input type="password" class="form-control" placeholder="Password">
-              <div class="input-group-append">
-                <div class="input-group-text">
-                  <span class="fas fa-lock"></span>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-8">
-                <!-- flot -->
-              </div>
-  
-              <div class="col-4">
-               
-                <a href="/home"type="submit" class="btn btn-primary btn-block">Sign In </a>
-              </div>
-  
-            </div>
-          </form>
-        </div>
-  
+  <section class="vh-100%">
+  <div class="container-fluid h-custom">
+    <div class="row d-flex justify-content-center " style="margin-top: 5%; padding: 10px;">
+      <div class="col-md-9 col-lg-6 col-xl-5">
+        <img src="../../assets/test.jpeg"
+          class="img-fluid" alt="Sample image">
+      </div>
+      <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+        <form>
+          <div class="text-center"> 
+            <i class="fa fa-user-circle" aria-hidden="true" style="font-size: 100px;"></i>
+            <h4>Admin</h4>
+          </div>
+          <div data-mdb-input-init class="form-outline mb-4">
+            <input type="email" id="form3Example3" class="form-control form-control-lg"
+              placeholder="Enter Username" />
+          </div>
+          <div data-mdb-input-init class="form-outline mb-3">
+            <input type="password" id="form3Example4" class="form-control form-control-lg"
+              placeholder="Enter password" />
+          </div>
+          <!-- <div class="text-center text-lg-start mt-4 pt-2">
+            <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg"
+              style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
+          </div> -->
+          <div class="text-center text-lg-start mt-4 pt-2">
+            <!-- <button data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3  " type="button" >Log in</button> -->
+            <a href="/home"type="submit" class="btn btn-primary btn-block">Sign In </a> 
+            <button data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">Register</button>                       
+            <a class="text-muted" href="#!" style="font-size: 20px; font-weight: 600;">Forgot password?</a>
+          </div>
+        </form>
       </div>
     </div>
   </div>
+</section>
 </template>
 
-<script>
-export default {
-  name: 'LoginPage',
-  data() {
-    return {
-
-    }
-  }
-}
-</script>
-
-<style lang="scss">
-
-</style>
